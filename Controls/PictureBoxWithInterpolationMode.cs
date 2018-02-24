221114ee9266d1f70439003ccf2172fd27a7a33e
Cuyler36 <jeremy96@vt.edu>
2018-02-23 19:10:39 -0500

    Move PictureBoxWithInterpolationMode Control to its Own File

---
+++ Controls/PictureBoxWithInterpolationMode.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace ACSE
+{
+    /// <summary>
+    /// Inherits from PictureBox; adds Interpolation Mode Setting
+    /// </summary>
+    public class PictureBoxWithInterpolationMode : PictureBox
+    {
+        public InterpolationMode InterpolationMode { get; set; }
+        public bool UseInternalInterpolationSetting { get; set; }
+
+        protected override void OnPaint(PaintEventArgs paintEventArgs)
+        {
+            if (paintEventArgs != null)
+            {
+                if (UseInternalInterpolationSetting)
+                    paintEventArgs.Graphics.InterpolationMode = (InterpolationMode)Properties.Settings.Default.ImageResizeMode;
+                else
+                    paintEventArgs.Graphics.InterpolationMode = InterpolationMode;
+                try { base.OnPaint(paintEventArgs); } catch { }
+            }
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs paintEventArgs)
+        {
+            if (UseInternalInterpolationSetting)
+                paintEventArgs.Graphics.InterpolationMode = (InterpolationMode)Properties.Settings.Default.ImageResizeMode;
+            else
+                paintEventArgs.Graphics.InterpolationMode = InterpolationMode;
+            try { base.OnPaintBackground(paintEventArgs); } catch { }
+        }
+    }
+}
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -4413,34 +4413,4 @@ namespace ACSE
 
         #endregion
     }
-
-    /// <summary>
-    /// Inherits from PictureBox; adds Interpolation Mode Setting
-    /// </summary>
-    public class PictureBoxWithInterpolationMode : PictureBox
-    {
-        public InterpolationMode InterpolationMode { get; set; }
-        public bool UseInternalInterpolationSetting { get; set; }
-
-        protected override void OnPaint(PaintEventArgs paintEventArgs)
-        {
-            if (paintEventArgs != null)
-            {
-                if (UseInternalInterpolationSetting)
-                    paintEventArgs.Graphics.InterpolationMode = (InterpolationMode)Properties.Settings.Default.ImageResizeMode; //InterpolationMode;
-                else
-                    paintEventArgs.Graphics.InterpolationMode = InterpolationMode;
-                try { base.OnPaint(paintEventArgs); } catch { }
-            }
-        }
-
-        protected override void OnPaintBackground(PaintEventArgs paintEventArgs)
-        {
-            if (UseInternalInterpolationSetting)
-                paintEventArgs.Graphics.InterpolationMode = (InterpolationMode)Properties.Settings.Default.ImageResizeMode; //InterpolationMode;
-            else
-                paintEventArgs.Graphics.InterpolationMode = InterpolationMode;
-            try { base.OnPaintBackground(paintEventArgs); } catch { }
-        }
-    }
 }
