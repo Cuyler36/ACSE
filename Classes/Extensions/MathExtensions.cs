cea858fb8f008b3641a46836a3a45dae7d15df49
Cuyler36 <jeremy96@vt.edu>
2018-08-23 02:14:55 -0400

    New ImageUtility class
    Will be removing old code in the future & switching to the classes used in this utility

---
+++ Classes/Extensions/MathExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System
+{
+    public static class MathExtensions
+    {
+        public static T Clamp<T>(this T value, T minimum, T maximum) where T : IComparable<T>
+        {
+            if (value.CompareTo(minimum) < 0) return minimum;
+            return value.CompareTo(maximum) > 0 ? maximum : value;
+        }
+    }
+}
